d00ef1a2947bca3cb3175a2e5ce567d7a159d623
iudini <hoiitzx@gmail.com>
2020-12-01 12:31:13 +0300

    Почта. [#184458] приведение к квадратичной сложности

--- chapter_001/src/main/java/ru/job4j/mail/Mail.java
+++ chapter_001/src/main/java/ru/job4j/mail/Mail.java
@@ -6,37 +6,52 @@ import java.util.Map;
 import java.util.Set;
 
 public class Mail {
+
+    /**
+     * Проверяем возвращаемую map на не пустоту, если пустая, то заполняем по первому ключу,
+     * при этом заполняем checkMap.
+     *
+     * Проверяем содержит ли checkMap email по данному ключу, после чего добавляем эти email в checkMap,
+     * если содержит, то записываем ключ, который содержит значение, в containedKey и перезаписываем еще раз checkMap.
+     *
+     * Если совпадений не было, то в результирующий массив добавляем значения по ключу, иначе по ключу containedKey
+     * дописываем значения.
+     */
+
     public static Map<String, Set<String>> merge(Map<String, Set<String>> map) {
         Map<String, Set<String>> result = new HashMap<>();
+        Map<String, String> checkMap = new HashMap<>();
         for (var key : map.keySet()) {
             if (result.isEmpty()) {
                 result.put(key, map.get(key));
+                for (String email : map.get(key)) {
+                    checkMap.put(email, key);
+                }
                 continue;
             }
 
             boolean mod = false;
-            for (var rsl : result.entrySet()) {
-                /** В новый set rslSet записываем значения из результирующей map result и запоминаем размер rslSetSize.
-                 * Далее в rslSet добавляем значения из исходной map.
-                 * Проверяем размер получившейся, если размер равен общему количеству элементов в двух set,
-                 * т.е. одинаковых элементов не было, значит в этот ключ мы пропускаем,
-                 * если размер не равен общему количеству, значит есть общие элементы и эти значения запишутся
-                 * в результирующий set в текущий ключ, после чего цикл прервется.
-                 * Если совпадений все размеры равны, то флаг модификации останется false, и после проверки,
-                 * всех ключей в результирующем set, в него будет добавлен еще один ключ, значение.
-                 */
-                Set<String> rslSet = new HashSet<>(rsl.getValue());
-                int rslSetSize = rslSet.size();
-                rslSet.addAll(map.get(key));
-                if (rslSetSize + map.get(key).size() != rslSet.size()) {
-                    rsl.setValue(rslSet);
+            String containedKey = "";
+            for (String email : map.get(key)) {
+                if (checkMap.containsKey(email)) {
+                    containedKey = checkMap.get(email);
                     mod = true;
                     break;
                 }
+                checkMap.put(email, key);
             }
 
             if (!mod) {
                 result.put(key, map.get(key));
+            } else {
+                for (String email : map.get(key)) {
+                    checkMap.put(email, containedKey);
+                }
+
+                Set<String> tmp = new HashSet<>();
+                tmp.addAll(result.get(containedKey));
+                tmp.addAll(map.get(key));
+                result.put(containedKey, tmp);
             }
         }
 
