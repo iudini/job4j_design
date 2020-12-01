e0396842c0f9ff5d18dba5b5885c5b75e4523344
iudini <hoiitzx@gmail.com>
2020-12-01 22:21:27 +0300

    1. Читаем файл конфигурации [#400006]

---
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            while (read.ready()) {
+                String line = read.readLine();
+                if (!line.isEmpty() && !line.startsWith("#") && line.contains("=")) {
+                    String[] lineSeparated = line.split("=");
+                    values.put(lineSeparated[0], lineSeparated[1]);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
