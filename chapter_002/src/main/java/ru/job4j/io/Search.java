64650f9668a635227ce3dff2d87b563b0aa0159c
iudini <hoiitzx@gmail.com>
2020-12-04 00:12:20 +0300

    4.1. Сканирование файловой системы. [#399997]

---
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,59 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, "js").forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    private static class SearchFiles implements FileVisitor<Path> {
+        Predicate<Path> predicate;
+        List<Path> paths = new ArrayList<>();
+
+        public SearchFiles(Predicate<Path> predicate) {
+            this.predicate = predicate;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if (predicate.test(file)) {
+                paths.add(file);
+            }
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            return CONTINUE;
+        }
+
+        public List<Path> getPaths() {
+            return paths;
+        }
+    }
+}
