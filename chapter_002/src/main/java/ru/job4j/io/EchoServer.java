469ec80749da39c7ef4d2325b81a3b301e39fc51
iudini <hoiitzx@gmail.com>
2020-12-19 15:07:06 +0300

    4. Slf4j - вывод exception. [#399995]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -1,11 +1,16 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean run = true;
             while (run) {
@@ -29,6 +34,8 @@ public class EchoServer {
                     out.write(message.getBytes());
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Exception IO", e);
         }
     }
 }
