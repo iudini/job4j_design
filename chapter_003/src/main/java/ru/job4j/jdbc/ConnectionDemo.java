2f23bd3a01def34b5569bd880bbb7f917aae9cce
iudini <hoiitzx@gmail.com>
2021-01-27 16:11:52 +0300

    0. Видео [#399908]

---
+++ chapter_003/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,28 @@
+package ru.job4j.jdbc;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Settings settings = new Settings();
+        ClassLoader loader = Settings.class.getClassLoader();
+        try (InputStream io = loader.getResourceAsStream("app.properties")) {
+            settings.load(io);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Class.forName("org.postgresql.Driver");
+        String url = settings.getValue("url");
+        String login = settings.getValue("login");
+        String password = settings.getValue("password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
