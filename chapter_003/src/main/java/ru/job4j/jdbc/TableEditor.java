e972b2d6296d7a2a992c89c714d6a43e465d2e69
iudini <hoiitzx@gmail.com>
2021-01-28 17:40:43 +0300

    0.1. Statement [#399905]

---
+++ chapter_003/src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,94 @@
+package ru.job4j.jdbc;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = properties.getProperty("url");
+            String password = properties.getProperty("password");
+            String login = properties.getProperty("login");
+            connection = DriverManager.getConnection(url, login, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("CREATE TABLE %s();", tableName);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("DROP TABLE %s;", tableName);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("ALTER TABLE %s ADD COLUMN %s %s;", tableName, columnName, type);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("ALTER TABLE %s DROP COLUMN %s;", tableName, columnName);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("ALTER TABLE %s RENAME COLUMN %s TO %s;", tableName, columnName, newColumnName);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getScheme(String tableName) throws SQLException {
+        StringBuilder scheme = new StringBuilder();
+        DatabaseMetaData metaData = connection.getMetaData();
+        try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
+            scheme.append(String.format("%-15s %-15s%n", "column", "type"));
+            while (columns.next()) {
+                scheme.append(String.format("%-15s %-15s%n",
+                        columns.getString("COLUMN_NAME"),
+                        columns.getString("TYPE_NAME")));
+            }
+        }
+        return scheme.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
