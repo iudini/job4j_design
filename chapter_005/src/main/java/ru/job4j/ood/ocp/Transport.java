e6c68e4bb6be885a40f3c5229f319dc3932a6c4d
iudini <hoiitzx@gmail.com>
2021-04-20 21:26:37 +0300

    0. Видео [#400043]

---
+++ chapter_005/src/main/java/ru/job4j/ood/ocp/Transport.java
@@ -0,0 +1,12 @@
+package ru.job4j.ood.ocp;
+
+public class Transport {
+
+    /*
+    при применение данного класса к автомобилям все ок, если применить его к самолетам, то придется
+    добавить вариант лететь
+     */
+    public String move() {
+        return "Go";
+    }
+}
